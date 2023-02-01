0528edce1a336659b8259332dd4985602ff95eb8
Mansoor riaaz <mansoor@ceksport.com>
2023-02-02 00:52:30 +0700

    fix: improve code documentation

--- src/application/service/cart/CartService.php
+++ src/application/service/cart/CartService.php
@@ -4,20 +4,33 @@ namespace Mansoor\TavernDddPhp\Application\Service\Cart;
 use Mansoor\TavernDddPhp\Domain\Aggregate\Product;
 
 class CartService{
+    /**
+     * @var array
+     */
     private array $products = [];
 
     private function __construct(){}
 
+    /**
+     * @return CartService
+     */
     public static function NewCart(): CartService{
         $cartService = new CartService();
         $cartService->products = [];
         return $cartService;
     }
 
+    /**
+     * @param Product $product
+     * @return void
+     */
     public function addProduct(Product $product):void{
         $this->products[] = $product;
     }
 
+    /**
+     * @return array
+     */
     public function getProducts(): array{
         return $this->products;
     }
